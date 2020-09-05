f7600dd4d715b32e8b42431188c2b182cce83543
Sujay Sathya <60563051+sujaysathya@users.noreply.github.com>
2020-09-05 06:19:34 +0530

    Update out_of_stock.py

--- out_of_stock.py
+++ out_of_stock.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 #from twilio.rest import Client
-#client = Client("AC619e99f5734354f0ec4cdf4d9e9e473a", "e932bfc09beaa95a6dae81d54dd8ac90")
+#client = Client("your_twillio_keys", "your_twillio_id")
 driver = webdriver.Chrome('./chromedriver')
 driver.get("https://www.amazon.in/dp/B08CFSS789/ref=cm_sw_r_wa_apa_i_.9MsFbJ5QHK36")
 print(driver.title)
