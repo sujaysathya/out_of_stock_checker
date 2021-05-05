b37ae22891721032c7880128001a9e5921823fc2
Sujay Sathya <60563051+sujaysathya@users.noreply.github.com>
2021-05-05 21:41:38 +0530

    Update out_of_stock.py

--- out_of_stock.py
+++ out_of_stock.py
@@ -4,7 +4,8 @@ from selenium.webdriver.common.keys import Keys
 
 #client = Client("your_twillio_keys", "your_twillio_id")
 driver = webdriver.Chrome('./chromedriver')
-driver.get("https://www.amazon.in/dp/B08CFSS789/ref=cm_sw_r_wa_apa_i_.9MsFbJ5QHK36")
+# enter items link below
+driver.get("https://www.amazon.in/b?ie=UTF8&node=21725163031")
 
 print(driver.title)
 el = driver.find_element_by_tag_name('body')
